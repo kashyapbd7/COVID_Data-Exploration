5f455103cccdeea091276111c9f5b14a66980a80
Kashyap <kashyapbd7@gmail.com>
2021-06-26 19:53:53 -0500

    Alterations, Generating Percent Deaths of US

--- SQL_Data_Cleaning.sql
+++ SQL_Data_Cleaning.sql
@@ -1,12 +1,25 @@
-/*Displaying All the data in table COVID Vaccinations*/
-SELECT *
-from dbo.CovidVaccinations
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+/*Displaying All the data in table COVID Deaths*/
 
 SELECT *
 from dbo.CovidDeaths
-ORDER  BY 2,3
 
 
+/*Data type for total_deaths and total_cases is varchar so converting 
+to Decimal for numerical calculations.*/
 
+Alter table CovidDeaths Alter Column total_deaths DECIMAL;
+Alter table CovidDeaths Alter Column total_cases DECIMAL;
+
+-- Looking at Total cases vs Total deaths (And percentage of death by cases)
+
+SELECT Location, date, total_cases, total_deaths, Cast((total_deaths/total_cases*100) as decimal(18,2)) as Percent_Deaths
+from dbo.CovidDeaths
+WHERE location like '%states%'
+order by 1,2
 
 
