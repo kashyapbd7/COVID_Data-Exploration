8f05adcbcc2e740b1db19a653815e75e8b82d6c3
Kashyap <kashyapbd7@gmail.com>
2021-06-27 08:18:58 -0500

    Total_case vs Population with PercentInfection

--- SQL_Data_Cleaning.sql
+++ SQL_Data_Cleaning.sql
@@ -17,9 +17,19 @@ Alter table CovidDeaths Alter Column total_cases DECIMAL;
 
 -- Looking at Total cases vs Total deaths (And percentage of death by cases)
 
-SELECT Location, date, total_cases, total_deaths, Cast((total_deaths/total_cases*100) as decimal(18,2)) as Percent_Deaths
+SELECT Location, date, total_cases, total_deaths, Cast((total_deaths*100/total_cases) as decimal(18,3)) as Percent_Deaths
 from dbo.CovidDeaths
 WHERE location like '%states%'
 order by 1,2
+
+-- Looking at total cases vs total population
+-- Shows what percentage of total population got COVID by Country(for eg, US)
+SELECT location, date, population, total_cases, CAST((total_cases*100/population) as decimal(18,3)) as Percent_cases
+FROM dbo.CovidDeaths
+WHERE location like '%states%'
+order by 2
+
+
+
 
 
