4c739505729e0b5b74cd48ec5614194280b18b97
Kashyap <kashyapbd7@gmail.com>
2021-06-27 17:56:33 -0500

    Filtering Continent from Country field

--- SQL_Data_Cleaning.sql
+++ SQL_Data_Cleaning.sql
@@ -20,6 +20,7 @@ Alter table CovidDeaths Alter Column total_cases DECIMAL;
 SELECT Location, date, total_cases, total_deaths, Cast((total_deaths*100/total_cases) as decimal(18,3)) as Percent_Deaths
 from dbo.CovidDeaths
 WHERE location like '%states%'
+    AND continent is NOT NULL
 order by 1,2
 
 -- Looking at total cases vs total population
@@ -28,11 +29,13 @@ order by 1,2
 SELECT location, date, population, total_cases, CAST((total_cases*100/population) as decimal(18,3)) as Percent_Infection
 FROM dbo.CovidDeaths
 WHERE location like '%states%'
+    AND continent is NOT NULL
 order by 2
 
 -- Looking at Maximum Infection Rate by Countries Total Population in Decreasing Order
 SELECT location, population, MAX(total_cases) as HighestCases, MAX(CAST((total_cases*100/population) as decimal(18,3))) as Max_Percent_Infection
 from dbo.CovidDeaths
+WHERE continent is NOT NULL
 GROUP By [location],population
 ORDER BY Max_Percent_Infection DESC
 
@@ -40,6 +43,7 @@ ORDER BY Max_Percent_Infection DESC
 -- Looking at Percent Deaths by Population of Countries Decreasing Order
 SELECT location, population, MAX(total_deaths) as Total_Death, MAX(CAST((total_deaths*100/population) as decimal(18,3))) as Percent_Deaths_by_Population
 from dbo.CovidDeaths
+WHERE continent is NOT NULL
 GROUP BY location, population
 ORDER BY Percent_Deaths_by_Population DESC
 
