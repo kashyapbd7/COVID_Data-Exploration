60e84b6a739007c293382c846b1c42a8829b7dac
Kashyap <kashyapbd7@gmail.com>
2021-06-27 18:03:51 -0500

    comment added

--- SQL_Data_Cleaning.sql
+++ SQL_Data_Cleaning.sql
@@ -1,8 +1,8 @@
 /*
 Covid 19 Data Exploration 
+
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
-
 /*Displaying All the data in table COVID Deaths*/
 
 SELECT *
